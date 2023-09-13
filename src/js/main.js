1b60c0c570236fd9f446d86e06732518406f1e95
algvana <algv.ana@gmail.com>
2023-09-13 14:10:33 +0200

    collapsables

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,10 @@ const designTitle = document.querySelector('.js-designTitle');
 const designContainer = document.querySelector('.js-designContainer');
 const refillContainer = document.querySelector('.js-refillContainer');
 const shareContainer = document.querySelector('.js-shareContainer');
-
-
-
+const refillTitle = document.querySelector('.js-refillTitle');
+const shareTitle = document.querySelector('.js-shareTitle');
+const createCard = document.querySelector('.js-create-card');
+const twitterCard = document.querySelector('.js-twitter-card');
 
 
 //datos
@@ -26,18 +27,49 @@ function collapShare(){
     shareContainer.classList.add('collapsed');
 }
 
+function showRefill(){
+    refillContainer.classList.remove('collapsed');
+}
 
-
+function collapDesign (){
+    designContainer.classList.add('collapsed');
+}
+function showShare () {
+    shareContainer.classList.remove('collapsed');
+}
+function showCreateCard(){
+    twitterCard.classList.remove('hidden');
+    createCard.classList.add('pulsed-grey');
+}
 
 function handleClickDesingTitle (event){
     showDesign();
     collapRefill();
     collapShare();
     console.log('oleeee');
-
 }
 
-
+function handleClickRefillTitle (event){
+    showRefill();
+    collapDesign();
+    collapShare();
+}
+function handleClickShareTitle (event){
+    showShare();
+    collapDesign();
+    collapRefill();
+}
+function handleClickCreateCard (event){
+    event.preventDefault();
+    showCreateCard();
+    createCard.classList.add('pulsed-grey');
+}
 //eventos
 
 designTitle.addEventListener ('click', handleClickDesingTitle);
+
+refillTitle.addEventListener ('click', handleClickRefillTitle);
+
+shareTitle.addEventListener ('click', handleClickShareTitle);
+
+createCard.addEventListener('click', handleClickCreateCard);
