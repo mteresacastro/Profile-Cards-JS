1585906b5f67bef7bc264e3cd9e7f0a791d3c74d
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-09-22 12:51:12 +0200

    image added apply

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,76 @@
-"use strict";
+'use strict';
 //query
-const designTitle = document.querySelector(".js-designTitle");
-const designContainer = document.querySelector(".js-designContainer");
-const refillContainer = document.querySelector(".js-refillContainer");
-const shareContainer = document.querySelector(".js-shareContainer");
-const refillTitle = document.querySelector(".js-refillTitle");
-const shareTitle = document.querySelector(".js-shareTitle");
-const createCard = document.querySelector(".js-create-card");
-const twitterCard = document.querySelector(".js-twitter-card");
-const form = document.querySelector('.js-form');
+const designTitle = document.querySelector('.js-designTitle');
+const designContainer = document.querySelector('.js-designContainer');
+const refillContainer = document.querySelector('.js-refillContainer');
+const shareContainer = document.querySelector('.js-shareContainer');
+const refillTitle = document.querySelector('.js-refillTitle');
+const shareTitle = document.querySelector('.js-shareTitle');
+const createCard = document.querySelector('.js-create-card');
+const twitterCard = document.querySelector('.js-twitter-card');
+const colors=document.querySelector('js-box-colors');
 ////////PREVIEW////////
 
-const resetButton = document.querySelector(".js-reset-button");
-const previewName = document.querySelector(".js-preview-name");
-const previewJob = document.querySelector(".js-preview-job");
-const previewPhone = document.querySelector(".js-preview-phone");
-const previewEmail = document.querySelector(".js-preview-email");
-const previewLinkedin = document.querySelector(".js-preview-linkedin");
-const previewGitHub = document.querySelector(".js-preview-github");
+const resetButton = document.querySelector('.js-reset-button');
+const previewName = document.querySelector('.js-preview-name');
+const previewJob = document.querySelector('.js-preview-job');
+const previewPhone = document.querySelector('.js-preview-phone');
+const previewEmail = document.querySelector('.js-preview-email');
+const previewLinkedin = document.querySelector('.js-preview-linkedin');
+const previewGitHub = document.querySelector('.js-preview-github');
 ////FORM
-const inputName = document.querySelector(".js-input-nameCompleted");
-const inputJob = document.querySelector(".js-input-job");
-const inputEmail = document.querySelector(".js-input-email");
-const inputPhone = document.querySelector(".js-input-phone");
-const inputLinkedin = document.querySelector(".js-input-linkedin");
-const inputGithub = document.querySelector(".js-input-github");
+const inputName = document.querySelector('.js-input-nameCompleted');
+const inputJob = document.querySelector('.js-input-job');
+const inputEmail = document.querySelector('.js-input-email');
+const inputPhone = document.querySelector('.js-input-phone');
+const inputLinkedin = document.querySelector('.js-input-linkedin');
+const inputGithub = document.querySelector('.js-input-github');
 ///COLORES
-const colorOne = document.querySelector(".js-color-1");
-const colorTwo = document.querySelector(".js-color-2");
-const colorThree = document.querySelector(".js-color-3");
-const borderContainer = document.querySelector(".js-container");
-const iconColors = document.querySelector(".js-icons");
-const iconColorsOne = document.querySelector(".js-icons-1");
-const iconColorsTwo = document.querySelector(".js-icons-2");
-const iconColorsThree = document.querySelector(".js-icons-3");
+const colorOne = document.querySelector('.js-color-1');
+const colorTwo = document.querySelector('.js-color-2');
+const colorThree = document.querySelector('.js-color-3');
+const borderContainer = document.querySelector('.js-container');
+const iconColors = document.querySelector('.js-icons');
+const iconColorsOne = document.querySelector('.js-icons-1');
+const iconColorsTwo = document.querySelector('.js-icons-2');
+const iconColorsThree = document.querySelector('.js-icons-3');
 
 //datos
 
 //funciones
 
 function showDesign() {
-  designContainer.classList.remove("collapsed");
+  designContainer.classList.remove('collapsed');
 }
 
 function collapRefill() {
-  refillContainer.classList.add("collapsed");
+  refillContainer.classList.add('collapsed');
 }
 
 function collapShare() {
-  shareContainer.classList.add("collapsed");
+  shareContainer.classList.add('collapsed');
 }
 
 function showRefill() {
-  refillContainer.classList.remove("collapsed");
+  refillContainer.classList.remove('collapsed');
 }
 
 function collapDesign() {
-  designContainer.classList.add("collapsed");
+  designContainer.classList.add('collapsed');
 }
 function showShare() {
-  shareContainer.classList.remove("collapsed");
+  shareContainer.classList.remove('collapsed');
 }
 function showCreateCard() {
-  twitterCard.classList.remove("hidden");
-  createCard.classList.add("pulsed-grey");
+  twitterCard.classList.remove('hidden');
+  createCard.classList.add('pulsed-grey');
 }
 
 function handleClickDesingTitle() {
   showDesign();
   collapRefill();
   collapShare();
-  console.log("oleeee");
+  console.log('oleeee');
 }
 
 function handleClickRefillTitle() {
@@ -86,29 +86,9 @@ function handleClickShareTitle() {
 function handleClickCreateCard(event) {
   event.preventDefault();
   showCreateCard();
-  createCard.classList.add("pulsed-grey");
+  createCard.classList.add('pulsed-grey');
 }
 /////////RESET///
-
-function handleResetInput(event) {
-  event.preventDefault();
-  inputName.value = "";
-  inputJob.value = "";
-  inputEmail.value = "";
-  previewEmail.href = "#";
-  inputPhone.value = "";
-  previewPhone.href = "#";
-  inputLinkedin.value = "";
-  previewLinkedin.href = "#";
-  inputGithub.value = "";
-  previewGitHub.href = "#";
-
-  previewName.innerHTML = "Nombre Apellido";
-  previewJob.innerHTML = "Font-end developer";
-
-  handleClickColorOne();
-  colorOne.checked = true;
-}
 const data = {
   palette: 1,
   name: '',
@@ -119,14 +99,36 @@ const data = {
   github: '',
   photo: '',
 };
+
+function handleResetInput(event) {
+  event.preventDefault(event);
+  inputName.value = '';
+  inputJob.value = '';
+  inputEmail.value = '';
+  previewEmail.href = '#';
+  inputPhone.value = '';
+  previewPhone.href = '#';
+  inputLinkedin.value = '';
+  previewLinkedin.href = '#';
+  inputGithub.value = '';
+  previewGitHub.href = '#';
+  previewName.innerHTML = 'Nombre Apellido';
+  previewJob.innerHTML = 'Font-end developer';
+
+  handleClickColorOne();
+  checked();
+}
+
+
 function updatePreview() {
+ 
   previewName.innerHTML = data.name;
   previewJob.innerHTML = data.job;
   previewPhone.href = `tel:${data.phone}`;
   previewEmail.href = `mailto:${data.email}`;
   previewLinkedin.href = `https://${data.linkedin}`;
   previewGitHub.href = `https://github.com/${data.github}`;
-};
+}
 
 
 function handleInputName() {
@@ -156,15 +158,13 @@ function handleInputLinkedin() {
 }
 function handleInputGithub() {
   const github = inputGithub.value;
-  const newGitHub = github.slice(1);
-  data.github = newGitHub;
+  data.github = github;
+  
   updatePreview();
 }
 ///PALETA COLORES///
-
-
-function handleClickColorOne(){
-  
+function handleClickColorOne() {
+  colorOne.getAttribute('checked');
   previewName.classList.remove('nameRed');
   previewName.classList.remove('nameBlue');
   borderContainer.classList.remove('containerRed');
@@ -184,7 +184,7 @@ function handleClickColorOne(){
   iconColorsThree.classList.add('liBlue');
 }
 
-function handleClickColorTwo(){
+function handleClickColorTwo() {
   previewName.classList.add('nameRed');
   previewName.classList.remove('nameBlue');
   borderContainer.classList.add('containerRed');
@@ -204,7 +204,7 @@ function handleClickColorTwo(){
   iconColorsThree.classList.remove('liGrey');
 }
 
-function handleClickColorThree(){
+function handleClickColorThree() {
   previewName.classList.add('nameBlue');
   borderContainer.classList.remove('containerRed');
   borderContainer.classList.remove('containerBlue');
@@ -236,6 +236,6 @@ inputEmail.addEventListener('input', handleInputEmail);
 inputPhone.addEventListener('input', handleInputPhone);
 inputLinkedin.addEventListener('input', handleInputLinkedin);
 inputGithub.addEventListener('input', handleInputGithub);
-colorOne.addEventListener('click',handleClickColorOne);
-colorTwo.addEventListener('click',handleClickColorTwo);
-colorThree.addEventListener('click',handleClickColorThree);
+colorOne.addEventListener('click', handleClickColorOne);
+colorTwo.addEventListener('click', handleClickColorTwo);
+colorThree.addEventListener('click', handleClickColorThree);
